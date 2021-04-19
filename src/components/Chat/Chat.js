3b6df3ec9eec8605989846be7fc615f18072e7fc
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-04-20 02:39:20 +0530

    first commit

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,10 +1,19 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  SearchOutlined,
+} from "@material-ui/icons";
 import MoreVert from "@material-ui/icons/MoreVert";
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 
 const Chat = () => {
+  const [input, setInput] = useState();
+
+  const sendMessage = () => {};
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -17,13 +26,58 @@ const Chat = () => {
           <IconButton>
             <SearchOutlined />
           </IconButton>
-          <IconButton>
+          {/* <IconButton>
             <AttachFile />
-          </IconButton>
+          </IconButton> */}
           <IconButton>
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">kshitij</span>MESSAGE
+          <span className="chat__timestamp">
+            {new Date() /* (message.timestamp?.toDate()) */
+              .toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">kshitij</span>MESSAGE
+          <span className="chat__timestamp">
+            {new Date() /* (message.timestamp?.toDate()) */
+              .toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">kshitij</span>MESSAGE
+          <span className="chat__timestamp">
+            {new Date() /* (message.timestamp?.toDate()) */
+              .toUTCString()}
+          </span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticon />
+        </IconButton>
+        <IconButton>
+          <AttachFile />
+        </IconButton>
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        <IconButton>
+          <Mic />
+        </IconButton>
       </div>
     </div>
   );
